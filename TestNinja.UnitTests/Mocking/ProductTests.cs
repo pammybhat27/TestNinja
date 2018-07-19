e0684d6ceec4e88627b35c82eede0d9cae6ee0c6
pammybhat27 <31756165+pammybhat27@users.noreply.github.com>
2018-07-20 08:52:18 +1000

    Avoiding pitfalls of mock
    Avoid using mock tests when it is not external dependency

---
+++ TestNinja.UnitTests/Mocking/ProductTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking {
+    [TestFixture]
+  public   class ProductTests {
+        [Test]
+        public void GetPrice_GoldCustomer_Apply30PercentDiscount()
+        {
+            //To get the price 
+            //where do we get the price from
+            //Product
+            //Instantiate the product
+            var productNewProduct = new Product();
+            productNewProduct.ListPrice = 100;
+
+            //What we need to check is for the get price that takes in customer object
+            //Isgold property of the customer object
+            //Whether it is true if it is 30 percent of the list price
+
+            var customer = new Customer();
+            customer.IsGold = true;
+
+            var newresult = productNewProduct.GetPrice(customer);
+
+            //var product = new Product {ListPrice = 100};
+
+            //var result = product.GetPrice(new Customer {IsGold = true});
+
+            Assert.That(newresult,Is.EqualTo(70));
+
+        }
+
+    }
+}
---
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests {
+    [TestFixture]
+    public class VideoServiceTests {
+        private VideoService _videoService;
+        private Mock<IFileReader> _fileReader;
+
+        [SetUp]
+        public void Setup() {
+            //We have an external dependency for filereader
+            //We create a mock object 
+            //we use the mock onject for the video service
+            _fileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_fileReader.Object);
+        }
+
+        [Test]
+        public void ReadVideoTitle_EmptyFile_returnError() {
+
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+
+            var result = _videoService.ReadVideoTitle();
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
