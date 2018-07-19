9820ab8b83180184c4f77681360bb80e6db0ffbb
Paramanand Bhat <HCM3@eurofins.com>
2018-07-19 20:59:07 +1000

    Method Parameters Dependency Injection
    For unit tests , pass a fake filereader

--- TestNinja.UnitTests/VideoServiceTests.cs
+++ TestNinja.UnitTests/VideoServiceTests.cs
@@ -3,8 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+using TestNinja.Mocking;
 
 namespace TestNinja.UnitTests {
+    [TestFixture]
    public class VideoServiceTests {
+        [Test]
+        public void ReadVideoTitle_EmptyFile_returnError()
+        {
+            var service = new VideoService();
+          var result =   service.ReadVideoTitle(new FakeFileReader());
+
+            Assert.That(result,Does.Contain("error").IgnoreCase);
+        }
     }
 }
