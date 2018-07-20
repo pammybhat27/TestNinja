6755d35723e21dd0e65889263b4ce4c3b57e422f
pammybhat27 <31756165+pammybhat27@users.noreply.github.com>
2018-07-20 14:08:50 +1000

    Adding Tests for Install helper
    New test class created for testing the mock , mock testing using moq

--- TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
+++ TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
@@ -1,11 +1,44 @@
 ﻿
+using System.Net;
+using Moq;
 using NUnit.Framework;
+using TestNinja.Mocking;
 
-namespace TestNinja.UnitTests.Mocking {
+namespace TestNinja.UnitTests.Mocking
+{
     [TestFixture]
-    public class InstallerHelperTests {
+    public class InstallerHelperTests
+    {
+        private Mock<IFileDownloader> _fileDownLoader;
 
+        private InstallerHelper _installerHelper;
+        //Create a mock of theIfile
 
+        [SetUp]
+        public void Setup()
+        {
+            _fileDownLoader = new Mock<IFileDownloader>();
+            _installerHelper = new InstallerHelper(_fileDownLoader.Object);
+        }
 
+        [Test]
+        public void DownloadInstaller_DownloadFails_ReturnsFalse()
+        {
+            _fileDownLoader.Setup(fd => fd.DownLoadFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws<WebException>();
+
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.False);
+
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadCompletes_ReturnsTrue()
+        {
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.True);
+        }
     }
 }
