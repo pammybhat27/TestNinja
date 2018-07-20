bbcb4ce359f349e51c61be6f95df4a21ff9390b6
pammybhat27 <31756165+pammybhat27@users.noreply.github.com>
2018-07-20 12:59:23 +1000

    Simple construction to create download file
    Method to download file that takes in string and path
    string is url and path is where contents will be stored

---
+++ TestNinja/Mocking/FileDownloader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.Mocking {
+    public class FileDownloader {
+
+        public void DownLoadFile(string url, string path)
+        {
+            var client = new WebClient();
+            client.DownloadFile(url,path);
+        }
+    }
+}
