0e5ac5039f86b285bab205c2bdbcc3f476a8458a
pammybhat27 <31756165+pammybhat27@users.noreply.github.com>
2018-07-20 15:01:38 +1000

    Employee storage
    Adding employee storage

--- TestNinja/Mocking/EmployeeStorage.cs
+++ TestNinja/Mocking/EmployeeStorage.cs
@@ -5,6 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TestNinja.Mocking {
-    public class EmployeeStorage {
+    public interface IEmployeeStorage
+    {
+        void DeleteEmployee(int id);
+    }
+
+    public class EmployeeStorage : IEmployeeStorage
+    {
+        private EmployeeContext _employeeContext;
+
+        public EmployeeStorage()
+        {
+            _employeeContext = new EmployeeContext();
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            
+            var employee =   _employeeContext.Employees.Find(id);
+            if (employee == null)
+                return;
+             
+            _employeeContext.Employees.Remove(employee);
+            _employeeContext.SaveChanges();
+           
+        }
+
     }
 }
